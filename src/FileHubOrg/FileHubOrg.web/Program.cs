06e73b792570ff98dae9372af1d4004c881aa92a
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-06 20:21:32 +0330

    Update Program.cs

--- src/FileHubOrg/FileHubOrg.web/Program.cs
+++ src/FileHubOrg/FileHubOrg.web/Program.cs
@@ -1,4 +1,65 @@
+﻿using FileHubOrg.Domain.Entities.User;
+using FileHubOrg.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Register services
+builder.Services.AddDbContext<FileHubOrgDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("FileHubOrg.Web"))
+    );
+
+// Identity services
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredUniqueChars = 1;
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+})
+.AddRoles<ApplicationRole>()
+.AddEntityFrameworkStores<FileHubOrgDbContext>()
+.AddDefaultTokenProviders();
+
+
+// Cookie settings
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.SlidingExpiration = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+// HttpContextAccessor for accessing HTTP context in services
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+// Web host environment for file uploads
+builder.Services.AddSingleton<IWebHostEnvironment>(builder.Environment);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
