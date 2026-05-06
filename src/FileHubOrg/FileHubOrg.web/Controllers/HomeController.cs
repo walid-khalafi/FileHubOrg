29be950136ba0b5dc47250962f946b408b87fb76
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-06 20:20:17 +0330

    Update HomeController.cs

--- src/FileHubOrg/FileHubOrg.web/Controllers/HomeController.cs
+++ src/FileHubOrg/FileHubOrg.web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
 using FileHubOrg.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileHubOrg.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
