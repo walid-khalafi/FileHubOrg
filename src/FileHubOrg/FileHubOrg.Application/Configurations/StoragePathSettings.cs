23710d266594f69e19905bf7d5a5f21c4f3e5dcd
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-07 19:52:00 +0330

    Create StoragePathSettings.cs

---
+++ src/FileHubOrg/FileHubOrg.Application/Configurations/StoragePathSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Application.Configurations
+{
+    public class StoragePathSettings
+    {
+        public string PhysicalPath { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
