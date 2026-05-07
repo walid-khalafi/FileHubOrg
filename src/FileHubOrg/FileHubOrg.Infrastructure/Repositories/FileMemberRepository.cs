d23303195a6b047c4b98761a515233ede891801c
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-07 18:56:44 +0330

    Create FileMemberRepository.cs

---
+++ src/FileHubOrg/FileHubOrg.Infrastructure/Repositories/FileMemberRepository.cs
@@ -0,0 +1,23 @@
+﻿using FileHubOrg.Domain.Entities.File;
+using FileHubOrg.Domain.Entities.User;
+using FileHubOrg.Domain.Interfaces;
+using FileHubOrg.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Infrastructure.Repositories
+{
+    public class FileMemberRepository : GenericRepository<FileMember>, IFileMemberRepository
+    {
+        private readonly FileHubOrgDbContext _context;
+        public FileMemberRepository(FileHubOrgDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
