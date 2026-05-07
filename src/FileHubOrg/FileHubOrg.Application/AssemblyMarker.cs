ee8e58de9a63ec72c992f1595662cfea57bf1385
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-07 19:14:53 +0330

    Create AssemblyMarker.cs

---
+++ src/FileHubOrg/FileHubOrg.Application/AssemblyMarker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Application
+{
+    /// <summary>
+    /// Marker class used to reference the Application assembly for AutoMapper scanning.
+    /// </summary>
+    public class AssemblyMarker { }
+}
