ac5844e8c28e5598272d0b75b34de49f67be7844
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-07 19:37:13 +0330

    Create RoleSeeder.cs

---
+++ src/FileHubOrg/FileHubOrg.Infrastructure/Persistence/Seeders/RoleSeeder.cs
@@ -0,0 +1,47 @@
+﻿using FileHubOrg.Domain.Entities.User;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Infrastructure.Persistence.Seeders
+{
+    public static class RoleSeeder
+    {
+        public static async Task SeedAsync(RoleManager<ApplicationRole> roleManager)
+        {
+            var roles = new List<ApplicationRole>
+            {
+                new ApplicationRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE",
+                    DisplayName = "Employee",
+                    Description = "Default Role for All users.",
+                  
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                },
+                new ApplicationRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    DisplayName = "System Admin",
+                    Description = "ACCESS TO ALL",
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                }
+            };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role.Name))
+                {
+                    await roleManager.CreateAsync(role);
+                }
+            }
+        }
+    }
+}
