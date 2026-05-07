a00cd148d1608ef08dc33e7b8e5ad4929e8079b3
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-07 16:01:20 +0330

    Create IJWTService.cs

---
+++ src/FileHubOrg/FileHubOrg.Application/Interfaces/IJWTService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Application.Interfaces
+{
+    public interface IJWTService
+    {
+        /// <summary>
+        /// Generate download token
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<string> GenerateDownloadJwtAsync(Guid fileId, string userId);
+        /// <summary>
+        /// validate download token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        ClaimsPrincipal? ValidateDownloadToken(string token);
+    }
+}
