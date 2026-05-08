e545b625155a0e92191471ee4cd198af199033e7
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-08 23:40:09 +0330

    Create IDepartmentService.cs

---
+++ src/FileHubOrg/FileHubOrg.Application/Interfaces/IDepartmentService.cs
@@ -0,0 +1,17 @@
+﻿using FileHubOrg.Domain.Entities.Organization;
+using FileHubOrg.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHubOrg.Application.Interfaces
+{
+    public interface IDepartmentService
+    {
+        Task<IReadOnlyList<Department>> GetDepartmentsAsync();
+        Task<Department> GetDepartmentsByIdAsync(Guid id);
+        Task<List<ApplicationUser>> GetDepartmentUsersAsync(Guid id);
+    }
+}
