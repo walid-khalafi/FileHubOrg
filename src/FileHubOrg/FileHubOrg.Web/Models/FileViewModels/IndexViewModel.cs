90c3b00aa09a0f4d27646015c15ff8b1681dac02
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-08 23:42:25 +0330

    Create IndexViewModel.cs

---
+++ src/FileHubOrg/FileHubOrg.Web/Models/FileViewModels/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileHubOrg.Web.Models.FileViewModels
+{
+    public class IndexViewModel
+    {
+        public Guid? labelId { get; set; }
+        public string? labelName { get; set; }
+       public List<Domain.Entities.File.FileMetaData> files { get; set; }
+    }
+}
