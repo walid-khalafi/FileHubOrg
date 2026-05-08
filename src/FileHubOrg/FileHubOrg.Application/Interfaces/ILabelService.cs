1003589de24fb48bbfd857f67840a7684c6a24ff
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-08 23:40:23 +0330

    Update ILabelService.cs

--- src/FileHubOrg/FileHubOrg.Application/Interfaces/ILabelService.cs
+++ src/FileHubOrg/FileHubOrg.Application/Interfaces/ILabelService.cs
@@ -10,6 +10,7 @@ namespace FileHubOrg.Application.Interfaces
     {
 
         Task<List<Domain.Entities.File.Label>> GetLabelsAsync(string userId);
+        Task<Domain.Entities.File.Label> GetLabelByIdAsync(Guid id);
         Task<bool> CreateLabelAsync(string userId, string labelName);
         Task<bool> RemoveLabelAsync(string userId, string labelId);
         Task<bool> UpdateLabelAsync(string userId, string labelId,string newName);
