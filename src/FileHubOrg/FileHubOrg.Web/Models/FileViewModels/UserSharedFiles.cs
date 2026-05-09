11b30e5d633080b7e4e4a1aeae36e08ce9f07c8f
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-10 02:47:09 +0330

    Create UserSharedFiles.cs

---
+++ src/FileHubOrg/FileHubOrg.Web/Models/FileViewModels/UserSharedFiles.cs
@@ -0,0 +1,13 @@
+﻿using FileHubOrg.Domain.Entities.File;
+
+namespace FileHubOrg.Web.Models.FileViewModels
+{
+    public class UserSharedFiles
+    {
+        public string userId { get; set; }
+        public string FullName { get; set; }
+        public Guid DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public List<FileMetaData> files { get; set; }
+    }
+}
