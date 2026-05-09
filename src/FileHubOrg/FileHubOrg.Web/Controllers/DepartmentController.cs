96d09de22d0f5aa63694561630dc733175b66dcc
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-10 01:09:29 +0330

    Update DepartmentController.cs

--- src/FileHubOrg/FileHubOrg.Web/Controllers/DepartmentController.cs
+++ src/FileHubOrg/FileHubOrg.Web/Controllers/DepartmentController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using FileHubOrg.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using FileHubOrg.Web.Models.DepartmentViewModels;
 namespace FileHubOrg.Web.Controllers
 {
+
     public class DepartmentController : Controller
     {
-        public IActionResult Index()
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
         {
-            return View();
+            _departmentService = departmentService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = new IndexViewModel() { Departments = new List<Domain.Entities.Organization.Department>() };
+            IReadOnlyList<Domain.Entities.Organization.Department> departments = await _departmentService.GetDepartmentsAsync();
+
+            model.Departments = departments.ToList();
+            return View(model);
         }
 
         public IActionResult Users()
