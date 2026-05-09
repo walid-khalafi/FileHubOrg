9ca1650a49bc83d69930ae68d845271373a761bf
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-10 01:09:25 +0330

    Create IndexViewModel.cs

---
+++ src/FileHubOrg/FileHubOrg.Web/Models/DepartmentViewModels/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FileHubOrg.Domain.Entities.Organization;
+
+namespace FileHubOrg.Web.Models.DepartmentViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Department> Departments { get; set; }
+    }
+}
