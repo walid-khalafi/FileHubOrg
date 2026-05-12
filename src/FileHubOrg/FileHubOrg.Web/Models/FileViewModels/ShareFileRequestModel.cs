2438d2626e9dc64bf647af36f9e5477cd654f2ae
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-12 22:54:33 +0330

    Create ShareFileRequestModel.cs

---
+++ src/FileHubOrg/FileHubOrg.Web/Models/FileViewModels/ShareFileRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FileHubOrg.Web.Models.FileViewModels
+{
+    public class ShareFileRequestModel
+    {
+        public Guid FileId { get; set; }
+        public string[] SharedToUserIds { get; set; }
+    }
+}
