c5dc9f3195c1201bb66737f5a89af156eb78d10c
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-13 12:06:49 +0330

    Create ShareListViewModel.cs

---
+++ src/FileHubOrg/FileHubOrg.Web/Models/FileViewModels/ShareListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FileHubOrg.Domain.Entities.Organization;
+using FileHubOrg.Domain.Entities.User;
+
+namespace FileHubOrg.Web.Models.FileViewModels
+{
+    public class ShareListViewModel
+    {
+        public Guid fileId { get; set; }
+        public List<Department>  Departments { get; set; }
+    }
+}
