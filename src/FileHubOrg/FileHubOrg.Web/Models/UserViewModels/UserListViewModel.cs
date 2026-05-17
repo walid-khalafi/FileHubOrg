0d01f81f6c09f33f44df76133f6a8703b8419c13
Walid Khalafi <walid.khalafi@icloud.com>
2026-05-17 22:38:33 +0330

    Update UserListViewModel.cs

--- src/FileHubOrg/FileHubOrg.Web/Models/UserViewModels/UserListViewModel.cs
+++ src/FileHubOrg/FileHubOrg.Web/Models/UserViewModels/UserListViewModel.cs
@@ -1,5 +1,6 @@
 using FileHubOrg.Domain.Entities.Organization;
 using FileHubOrg.Domain.Entities.User;
+using System.Collections.Generic;
 
 namespace FileHubOrg.Web.Models.UserViewModels
 {
@@ -7,5 +8,6 @@ namespace FileHubOrg.Web.Models.UserViewModels
     {
         public IReadOnlyList<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
         public IReadOnlyList<Department> Departments { get; set; } = new List<Department>();
+        public IReadOnlyDictionary<string, int> UserFileCounts { get; set; } = new Dictionary<string, int>();
     }
 }
